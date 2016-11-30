1eeda6125b116102dea776076649f28430aa6642
Keith Clark <geauxlsutigers24@gmail.com>
2016-11-30 17:14:45 +0000

    generated pages controller/views and set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  root 'page#home'
+
+  get 'page/home'
+
+  get 'page/about'
+
+  get 'page/greatest_hits'
+
   resources :products
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
